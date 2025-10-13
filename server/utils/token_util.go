7c4dfa892daef5e7d1a4d145d595eafa3db5d127
aog-developer <elisandil88@gmail.com>
2025-10-13 15:39:34 +0200

    feat: Add middleware for authentication

--- server/utils/token_util.go
+++ server/utils/token_util.go
@@ -2,11 +2,13 @@ package utils
 
 import (
 	"context"
+	"errors"
 	"os"
 	"time"
 
 	"server/database"
 
+	"github.com/gin-gonic/gin"
 	jwt "github.com/golang-jwt/jwt/v5"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
@@ -61,6 +63,39 @@ func UpdateAllTokens(userId, token, refreshToken string) (err error) {
 	}
 
 	return nil
+}
+
+func GetAccessToken(c *gin.Context) (string, error) {
+	authHeader := c.Request.Header.Get("Authorization")
+	if authHeader == "" {
+		return "", errors.New("authorization header is required")
+	}
+
+	tokenString := authHeader[len("Bearer "):]
+	if tokenString == "" {
+		return "", errors.New("bearer token is required")
+	}
+
+	return tokenString, nil
+}
+
+func ValidateToken(tokenString string) (*SignedDetails, error) {
+	claims := &SignedDetails{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(SecretKey), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, err
+	}
+	if claims.ExpiresAt.Time.Before(time.Now()) {
+		return nil, errors.New("token has expired")
+	}
+
+	return claims, nil
 }
 
 // ------------------------------------------------------------------------------------
