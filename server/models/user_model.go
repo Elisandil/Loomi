c4fa9ede9609662b9b03a85266b61810bb5a55f0
aog-developer <elisandil88@gmail.com>
2025-10-13 12:05:27 +0200

    bugfix: Mapping of the genres corrected

--- server/models/user_model.go
+++ server/models/user_model.go
@@ -6,13 +6,6 @@ import (
 	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
-type Role string
-
-const (
-	RoleAdmin = "ADMIN"
-	RoleUser  = "USER"
-)
-
 type User struct {
 	ID              bson.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
 	UserID          string        `bson:"user_id" json:"user_id"`
@@ -20,12 +13,12 @@ type User struct {
 	LastName        string        `bson:"last_name" json:"last_name" validate:"required,min=2,max=100"`
 	Email           string        `bson:"email" json:"email" validate:"required,email"`
 	Password        string        `bson:"password" json:"password" validate:"required,min=8"`
-	Role            Role          `bson:"role" json:"role" validate:"oneof=ADMIN USER"`
+	Role            string        `bson:"role" json:"role" validate:"oneof=ADMIN USER"`
 	CreatedAt       time.Time     `bson:"created_at" json:"created_at"`
 	UpdatedAt       time.Time     `bson:"updated_at" json:"updated_at"`
 	Token           string        `bson:"token" json:"token"`
 	RefreshToken    string        `bson:"refresh_token" json:"refresh_token"`
-	FavouriteGenres []Genre       `bson:"genres" json:"genres" validate:"dive"`
+	FavouriteGenres []Genre       `bson:"favourite_genres" json:"favourite_genres" validate:"dive"`
 }
 
 type UserLogin struct {
@@ -39,6 +32,8 @@ type UserResponse struct {
 	FirstName       string  `json:"first_name"`
 	LastName        string  `json:"last_name"`
 	Email           string  `json:"email"`
-	Role            Role    `json:"role"`
+	Role            string  `json:"role"`
+	Token           string  `json:"token"`
+	RefreshToken    string  `json:"refresh_token"`
 	FavouriteGenres []Genre `json:"favourite_genres"`
 }
