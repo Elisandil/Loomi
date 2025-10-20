bb9e418ff17026115ff3fa093fce09a298836a24
aog-developer <elisandil88@gmail.com>
2025-10-20 08:27:43 +0200

    Refactor: New design

--- client/src/components/movies/Movies.jsx
+++ client/src/components/movies/Movies.jsx
@@ -1,13 +1,23 @@
 import Movie from '../movie/Movie'
+import './Movies.css'
 
 const Movies = ({movies, message}) => {
     return (
-        <div className="container mt-4">
-            <div className="row">
-                {movies && movies.length > 0
-                    ? movies.map((movie) => (<Movie key={movie._id} movie={movie} />))
-                    : <h2>{message}</h2>
-                }
+        <div className="movies-container">
+            <div className="movies-grid-wrapper">
+                {movies && movies.length > 0 ? (
+                    movies.map((movie) => (
+                        <Movie key={movie._id} movie={movie} />
+                    ))
+                ) : (
+                    <div className="no-movies-message">
+                        <div className="no-movies-icon">🎬</div>
+                        <h2 className="no-movies-text">{message}</h2>
+                        <p className="no-movies-subtext">
+                            Check back later for new releases!
+                        </p>
+                    </div>
+                )}
             </div>
         </div>
     )
